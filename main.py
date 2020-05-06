705f2e78e2453b90a2c6d7e7bbfefad2f0f5c0db
Luke Zielinski <lpz@chromium.org>
2020-05-06 13:33:13 -0400

    Add metrics for counting statuses of open PRs.
    This includes metric tracking all status "contexts" (such as
    TaskCluster) and their statuses, as well as counting how many PRs have
    no checks at all (which should be actionable immediately).
    
    This also introduces a MonitorState object for sharing data between
    metrics, to avoid having to re-query things.

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import json
 import logging
 import sys
 import time
+from collections import defaultdict
 from datetime import datetime
 from datetime import timezone
 
@@ -26,6 +27,7 @@ GCLOUD_PROJECT_ID = "wptmon"
 DESCRIPTOR_PROJECT_NAME = "projects/wptmon"
 RUNS_REQ_TIMEOUT_SECONDS = 5
 RUNS_MAX_AGE_SECONDS = 24 * 60 * 60  # 1 day
+OPEN_PRS_MAX_AGE_SECONDS = 7 * 24 * 60 * 60  # 1 week
 ERROR_RESULT = -1
 
 RECENT_STABLE_RUNS_FETCH_URL = "https://wpt.fyi/api/runs?labels=master,stable&products=chrome,firefox,safari&max-count=1"
@@ -34,6 +36,22 @@ RECENT_EXPERIMENTAL_RUNS_FETCH_URL = "https://wpt.fyi/api/runs?labels=master,exp
 METRIC_TYPE_RECENT_STABLE_RUNS = "custom.googleapis.com/wpt.fyi/recent_stable_runs"
 METRIC_TYPE_RECENT_EXPERIMENTAL_RUNS = "custom.googleapis.com/wpt.fyi/recent_experimental_runs"
 METRIC_TYPE_OPEN_PRS = "custom.googleapis.com/wpt-github/open_prs"
+METRIC_TYPE_OPEN_PR_STATUSES = "custom.googleapis.com/wpt-github/open_pr_status"
+METRIC_TYPE_OPEN_PR_NO_CHECKS = "custom.googleapis.com/wpt-github/open_pr_no_checks"
+
+
+class MonitorState():
+  """This is a state object for holding artifacts during metrics gathering.
+
+  Many of the APIs we call to get metrics data require HTTP calls or
+  heavy-weight processing, so it's useful to be able to save intermediate
+  data from processing one metric to later use for processing another metric.
+  """
+  def __init__(self):
+    # The github.PaginatedList.PaginatedList object containing the set of open
+    # pull requests from github. Note that accessing this issues more HTTP
+    # requests.
+    self.github_open_prs = None
 
 
 def _get_github_token():
@@ -106,6 +124,61 @@ def create_and_write_metric_open_prs(num_open_prs):
   series.resource.labels["task_id"] = "wpt-github"
   point = series.points.add()
   point.value.int64_value = num_open_prs
+  point.interval.end_time.seconds = int(time.time())
+  client.create_time_series(DESCRIPTOR_PROJECT_NAME, [series])
+
+
+def create_and_write_metric_open_pr_no_checks(num_prs_with_no_checks):
+  client = monitoring_v3.MetricServiceClient()
+  descriptor = monitoring_v3.types.MetricDescriptor()
+  descriptor.type = METRIC_TYPE_OPEN_PR_NO_CHECKS
+  descriptor.value_type = monitoring_v3.enums.MetricDescriptor.ValueType.INT64
+  descriptor.metric_kind = monitoring_v3.enums.MetricDescriptor.MetricKind.GAUGE
+  descriptor.description = "Number of open Github pull requests with no checks"
+  client.create_metric_descriptor(DESCRIPTOR_PROJECT_NAME, descriptor)
+
+  series = monitoring_v3.types.TimeSeries()
+  series.metric.type = METRIC_TYPE_OPEN_PR_NO_CHECKS
+  series.resource.type = "generic_task"
+  series.resource.labels["namespace"] = "wpt"
+  series.resource.labels["location"] = "us-east1"
+  series.resource.labels["job"] = "wpt-github"
+  series.resource.labels["task_id"] = "wpt-github"
+  point = series.points.add()
+  point.value.int64_value = num_prs_with_no_checks
+  point.interval.end_time.seconds = int(time.time())
+  client.create_time_series(DESCRIPTOR_PROJECT_NAME, [series])
+
+
+def create_metric_open_pr_statuses():
+  client = monitoring_v3.MetricServiceClient()
+  descriptor = monitoring_v3.types.MetricDescriptor()
+  descriptor.type = METRIC_TYPE_OPEN_PR_STATUSES
+  descriptor.value_type = monitoring_v3.enums.MetricDescriptor.ValueType.INT64
+  descriptor.metric_kind = monitoring_v3.enums.MetricDescriptor.MetricKind.GAUGE
+  descriptor.description = "Check statuses for open pull requests in the WPT repository"
+  client.create_metric_descriptor(DESCRIPTOR_PROJECT_NAME, descriptor)
+
+
+def write_metric_open_pr_statuses(check_name, check_status, count):
+  """Writes a single point to the 'open pr statuses' metric.
+
+  :arg check_name: str the name of the check that ran on the PR
+  :arg check_status: str the status of the check (eg: success, failure, error)
+  :arg count: int the number of PRs that had this check with this status.
+  """
+  client = monitoring_v3.MetricServiceClient()
+  series = monitoring_v3.types.TimeSeries()
+  series.metric.type = METRIC_TYPE_OPEN_PR_STATUSES
+  series.resource.type = "generic_task"
+  series.resource.labels["namespace"] = "wpt"
+  series.resource.labels["location"] = "us-east1"
+  # We are mis-using labels here because cloud monitoring won't let us define
+  # our own labels.
+  series.resource.labels["job"] = check_name
+  series.resource.labels["task_id"] = check_status
+  point = series.points.add()
+  point.value.int64_value = count
   point.interval.end_time.seconds = int(time.time())
   client.create_time_series(DESCRIPTOR_PROJECT_NAME, [series])
 
@@ -210,7 +283,7 @@ def get_recent_experimental_runs():
   return "Recent experimental runs: %s" % recent_browsers
 
 
-def get_num_open_prs():
+def get_num_open_prs(mon_state):
   logging.info("Counting open github PRs")
   gh_token = _get_github_token()
   if not gh_token:
@@ -218,7 +291,9 @@ def get_num_open_prs():
 
   github = Github(gh_token)
   wpt_repo = github.get_repo("web-platform-tests/wpt")
-  num_open_prs = wpt_repo.get_pulls(state="open", base="master").totalCount
+  mon_state.github_open_prs = wpt_repo.get_pulls(state="open", base="master",
+                                                 sort="updated", direction="desc")
+  num_open_prs = mon_state.github_open_prs.totalCount
   logging.info("Finished counting open PRs: %d" % num_open_prs)
 
   try:
@@ -228,11 +303,74 @@ def get_num_open_prs():
   return "Open PRs: %d" % num_open_prs
 
 
+def get_open_pr_statuses(mon_state):
+  logging.info("Finding statuses for open PRs")
+  # We require that open PRs have already been identified.
+  # TODO: make this capable of rebuilding open PRs list if needed
+  assert mon_state.github_open_prs is not None
+
+  # Status types is a 2-level dict mapping context->state->count, making it a
+  # dict(str->dict(str->int))
+  status_dict = defaultdict(lambda: defaultdict(int))
+  # Keep track of the PR numbers that have no checks.
+  prs_with_no_checks = []
+
+  for open_pr in mon_state.github_open_prs:
+    # The timestamps reported by github are in UTC, so add the tzinfo.
+    pr_update_time_utc = open_pr.updated_at.replace(tzinfo=timezone.utc)
+    pr_age_seconds = (datetime.now(tz=timezone.utc) - pr_update_time_utc).total_seconds()
+    if pr_age_seconds >= OPEN_PRS_MAX_AGE_SECONDS:
+      logging.info("PR %d last updated on %s which exceeds the max, skipping the rest"
+                   % (open_pr.number, open_pr.updated_at.strftime("%c%Z")))
+      break
+
+    # Commits are listed in order of increasing timestamp, meaning the last one
+    # is the most-recent one.
+    latest_commit = open_pr.get_commits()[open_pr.commits - 1]
+    # We try to dedupe the statuses for a given commit. This API seems to list
+    # the history of statuses for this commit, so you often see a "pending" check
+    # followed by that same check in a "success" state. We only want to keep the
+    # success in that example.
+    # Statuses are listed in reverse-chronological order, with the latest at the
+    # top of the list, so we traverse backwards to follow the timeline of events
+    # and overwrite status for any check that repeats.
+    deduped_dict = defaultdict(str)
+    for commit_status in latest_commit.get_statuses().reversed:
+      deduped_dict[commit_status.context] = commit_status.state
+    logging.info("PR %d has check status %s" % (open_pr.number, deduped_dict))
+    if not deduped_dict:
+      # Empty dict means there are no checks on this PR
+      prs_with_no_checks.append(open_pr.number)
+    # Add the deduped statuses for this PR to the combined list
+    for context, state in deduped_dict.items():
+      status_dict[context][state] += 1
+
+  try:
+    create_and_write_metric_open_pr_no_checks(len(prs_with_no_checks))
+  except g_exceptions.InvalidArgument as e:
+    logging.error("Failed to PRs-with-no-checks metric, skipping: %s" % e)
+
+  create_metric_open_pr_statuses()
+  for context, state_dict in status_dict.items():
+    for state, count in state_dict.items():
+      print("Trying to write a metric for %s %s %d" % (context, state, count))
+      try:
+        write_metric_open_pr_statuses(context, state, count)
+      except g_exceptions.InvalidArgument as e:
+        logging.error("Failed to PR status metric, skipping: %s" % e)
+
+  logging.info("Got combined PR statuses, dict=%s", status_dict)
+  logging.info("PRs with no checks, %s", prs_with_no_checks)
+  return "Open PR statuses: %s<br>\nOpen PRs with no checks: [%d]%s" \
+         % (status_dict, len(prs_with_no_checks), prs_with_no_checks)
+
 @app.route("/")
 def get_all_metrics():
+  mon_state = MonitorState()
   result = get_recent_stable_runs()
   result += "<br>\n" + get_recent_experimental_runs()
-  result += "<br>\n" + get_num_open_prs()
+  result += "<br>\n" + get_num_open_prs(mon_state)
+  result += "<br>\n" + get_open_pr_statuses(mon_state)
   return result
 
 
